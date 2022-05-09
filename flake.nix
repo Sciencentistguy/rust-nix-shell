73c0de6d74b5307b154d3900d20395fa5d2da27c
Jamie Quigley <jamie@quigley.xyz>
2022-05-09 12:42:41 +0100

    Specify package name in flake

--- flake.nix
+++ flake.nix
@@ -16,29 +16,31 @@
   }:
     {
       overlay = final: prev: {
-        generic-nix-shell = self.packages.${prev.system}.default;
+        generic-rust-shell = self.packages.${prev.system}.default;
       };
     }
     // flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib;
-        generic-nix-shell = {
+        generic-rust-shell = {
           lib,
           rustPlatform,
         }:
           rustPlatform.buildRustPackage {
-            name = "generic-nix-shell";
+            name = "generic-rust-shell";
             src = lib.cleanSource ./.;
             cargoLock.lockFile = ./Cargo.lock;
             meta = with lib; {
               license = licenses.mpl20;
-              homepage = "https://github.com/Sciencentistguy/generic-nix-shell";
+              homepage = "https://github.com/Sciencentistguy/generic-rust-shell";
               platforms = platforms.all;
             };
           };
       in {
-        packages.default = pkgs.callPackage generic-nix-shell {};
+        packages.generic-rust-shell = pkgs.callPackage generic-rust-shell {};
+        packages.default = self.packages.${system}.generic-rust-shell;
+
         devShells.default = self.packages.${system}.default.overrideAttrs (super: {
           nativeBuildInputs = with pkgs;
             super.nativeBuildInputs
